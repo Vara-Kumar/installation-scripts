55411e43ca46e77ba22624fe799dee1e56efa4bb
Vara-Kumar <142789755+Vara-Kumar@users.noreply.github.com>
2024-02-26 13:02:06 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,9 +1,8 @@
+#!/bin/bash
 yum install java-17 -y 
-
 wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo 
-
 rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key 
-
 yum install jenkins -y 
-
 systemctl restart jenkins 
+echo "Initial admin password:"
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword
